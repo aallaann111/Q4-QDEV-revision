e64106175de48f2b117dde32a0e8cf8117970d1b
alan terrier <alan.terrier@etu.umontpellier.fr>
2023-12-19 14:04:29 +0100

    question 6

--- StrategyObserverStudent/src/main/java/antipattern/AudioCompressionStrategy.java
+++ StrategyObserverStudent/src/main/java/antipattern/AudioCompressionStrategy.java
@@ -1,14 +1,66 @@
 package antipattern;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class AudioCompressionStrategy implements DataCompressionStrategy {
     @Override
     public String compress(Data data) {
-        if (data instanceof AudioData) {
-            AudioData audioData = (AudioData) data;
-            // Logique de compression audio
-            // ...
-            return "Compressed audio: " + audioData.getFile().getName();
+        String result = "./data/compressed/wav/c-" + data.getFile().getName();
+        try {
+            FileInputStream inputStream = new FileInputStream(data.getFile().getAbsolutePath());
+
+            FileOutputStream outputStream = new FileOutputStream(result);
+
+
+            byte[] buffer = new byte[4096];
+            int length;
+
+            while (true) {
+
+                if (!((length = inputStream.read(buffer)) > 0)) break;
+                outputStream.write(buffer, 0, length);
+
+
+            }
+
+
+            inputStream.close();
+
+            outputStream.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return "";
+        updateAudio(result);
+        return result;
     }
+
+    public void updateAudio(String fileName) {         // Créer un objet AudioInputStream à partir du fichier audio WAV
+        // Code d'observation pour les audios
+        if (fileName.contains("/wav/")) {
+            LocalTime now = LocalTime.now();
+
+            // Formater l'heure dans le format souhaité (hh:mm:ss)
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String formattedTime = now.format(formatter);
+            String content = formattedTime;
+
+
+            System.out.println("-----------------------------------------------------------------------");
+            System.out.println("Observation des audios...");
+
+            content += " ::Fichier compressé : " + fileName;
+            FileWriter.writeToFile("./data/compressed/wav/AudioCompressionObserver.txt", content);
+            System.out.println("-----------------------------------------------------------------------");
+
+        }
+    }
+
 }
--- StrategyObserverStudent/src/main/java/antipattern/VideoCompressionStrategy.java
+++ StrategyObserverStudent/src/main/java/antipattern/VideoCompressionStrategy.java
@@ -1,14 +1,66 @@
 package antipattern;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class VideoCompressionStrategy implements DataCompressionStrategy {
     @Override
     public String compress(Data data) {
-        if (data instanceof VideoData) {
-            VideoData videoData = (VideoData) data;
-            // Logique de compression vidéo
-            // ...
-            return "Compressed video: " + videoData.getFile().getName();
+        String result = "./data/compressed/mp4/c-" + data.getFile().getName();
+        try {
+            FileInputStream inputStream = new FileInputStream(data.getFile().getAbsolutePath());
+
+            FileOutputStream outputStream = new FileOutputStream(result);
+
+
+            byte[] buffer = new byte[4096];
+            int length;
+
+            while (true) {
+
+                if (!((length = inputStream.read(buffer)) > 0)) break;
+                outputStream.write(buffer, 0, length);
+
+
+            }
+
+
+            inputStream.close();
+
+            outputStream.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return "";
+        updateVideo(result);
+        return result;
     }
+
+    public void updateVideo(String fileName) {
+        // Code d'observation pour les videos
+        if (fileName.contains("/mp4/")) {
+            LocalTime now = LocalTime.now();
+
+
+            // Formater l'heure dans le format souhaité (hh:mm:ss)
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String formattedTime = now.format(formatter);
+            String content = formattedTime;
+
+
+            System.out.println("-----------------------------------------------------------------------");
+            System.out.println("Observation des audios...");
+
+            content += " ::Fichier compressé : " + fileName;
+            FileWriter.writeToFile("./data/compressed/mp4/VideoCompressionObserver.txt", content);
+            System.out.println("-----------------------------------------------------------------------");
+        }
+    }
+
 }
